1d1e186e8349c3dc61c3bab02ba4a310421eeb78
Kaisar Toktarkhan <kaisarmaxat@gmail.com>
2020-05-19 18:14:59 +0600

    Started to do image cards

---
+++ src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Card.css";
+
+const Card = (props) => {
+  return (
+    <div className="Card">
+      <img src={props.image} alt="" className="Card_img" />
+      <div className="Card_info">
+        <img src={props.avatar} alt="" className="Card_info_image" />
+        <p className="Card_info_name">{props.name}</p>
+        <p className="Card_info_instagram">@{props.instagram}</p>
+        <div className="Card_buttons">
+          <span className="Card_button card_button_favourite"></span>
+          <span className="Card_button card_button_wide"></span>
+          <span className="Card_button card_button_download"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Collection.css";
 import mainArrow from "../../assets/img/main-arrow.svg";
+import Card from "../Card/Card";
 
 const Collection = (props) => {
   let collectionList = (
@@ -13,11 +14,22 @@ const Collection = (props) => {
     collectionList = (
       <div className="Collection">
         <div className="container Collection_wrapper">
-          {props.photos.map((item) => {
+          {/* {props.photos.map((item) => {
             return (
               <div className="Collection_img_wrapper" key={item.id}>
                 <img className="Collection_img" src={item.urls.small} alt="" />
               </div>
+            );
+          })} */}
+          {props.photos.map((item) => {
+            return (
+              <Card
+                image={item.urls.small}
+                key={item.id}
+                name={item.user.name}
+                instagram={item.user.instagram_username}
+                avatar={item.user.profile_image.small}
+              />
             );
           })}
         </div>
